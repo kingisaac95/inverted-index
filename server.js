8e8f568c4b25e6ddb5b539a5442740a248828ebd
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2017-03-24 09:06:24 +0100

    added server.js, edited package.json

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}./index.html`);
+});
+
+app.use(express.static(path.join(__dirname, 'app')));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => `Listening on port ${port}`);
