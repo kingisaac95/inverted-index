eb6a3d68ac396da649119e594d669df80e0728b5
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2017-03-24 11:47:16 +0100

    edit(changed port config)

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-// app.use(express.static(path.join(__dirname, './')));
-// app.set('port', (process.env.PORT || 8080));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(`${__dirname}/index.html`);
-// });
-// // const port = process.env.PORT || 5000;
-// app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`));
-
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -22,11 +9,11 @@ const port = process.env.PORT || 8080;
 
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, './')));
 
 // set the home page route
 app.get('/', (req, res) => {
-  res.render('index');
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 app.listen(port, () => {
