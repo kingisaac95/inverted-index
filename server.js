67df24d701bab40b4b131ad7c1fe8bca771e5f5e
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2017-03-24 11:33:12 +0100

    edit (updated port)

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// app.use(express.static(path.join(__dirname, './')));
+// app.set('port', (process.env.PORT || 8080));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(`${__dirname}/index.html`);
+// });
+// // const port = process.env.PORT || 5000;
+// app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`));
+
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 8080;
+
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(`${__dirname}/public`));
+
+// set the home page route
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+  res.render('index');
 });
 
-app.use(express.static(path.join(__dirname, './')));
-app.set('port', (process.env.PORT || 8080));
-
-// const port = process.env.PORT || 5000;
-app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`));
+app.listen(port, () => {
+  console.log(`Our app is running on http://localhost:${port}`);
+});
